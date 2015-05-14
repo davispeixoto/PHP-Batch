ca24d33a35bfb9e632020232f1c50df45ac42a2b
Davis L P Peixoto <davis.peixoto@gmail.com>
2015-05-14 15:08:05 -0300

    pharing stuff and libraries

---
+++ src/Davispeixoto/PhpBatch/Application/Command.php
@@ -0,0 +1,20 @@
+<?php namespace Davispeixoto\PhpBatch\Application;
+
+/**
+ * Class Command
+ * @package Davispeixoto\PhpBatch\Application
+ */
+
+/**
+ * Created by Davis Peixoto <davis.peixoto@gmail.com>.
+ * Date: 5/14/15
+ * Time: 2:47 PM
+ * Powered By PhpStorm
+ */
+
+use Symfony\Component\Console\Command as Comm;
+
+class Command extends Comm
+{
+
+}
