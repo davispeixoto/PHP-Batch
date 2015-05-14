145f643d1045a327ecbb9de87d30b9317fa33d5d
Davis L P Peixoto <davis.peixoto@gmail.com>
2015-05-14 17:03:23 -0300

    on going

---
+++ src/Davispeixoto/PhpBatch/Contracts/ItemProcessorInterface.php
@@ -0,0 +1,18 @@
+<?php namespace Davispeixoto\PhpBatch\Contracts;
+
+/**
+ * Interface ItemProcessorInterface
+ * @package Davispeixoto\PhpBatch\Contracts
+ */
+
+/**
+ * Created by Davis Peixoto <davis.peixoto@gmail.com>.
+ * Date: 5/14/15
+ * Time: 4:18 PM
+ * Powered By PhpStorm
+ */
+
+interface ItemProcessorInterface
+{
+    public function process();
+}
--- src/Davispeixoto/PhpBatch/Step/Step.php
+++ src/Davispeixoto/PhpBatch/Step/Step.php
@@ -1,14 +1,16 @@
-<?php
+<?php namespace Davispeixoto\PhpBatch\Step;
+
 /**
- * Created by PhpStorm.
- * User: Dave
- * Date: 01/05/2015
- * Time: 04:19
+ * Class Step
+ * @package Davispeixoto\PhpBatch\Step
  */
 
-namespace Davispeixoto\PhpBatch\Step;
-
-
+/**
+ * Created by Davis Peixoto <davis.peixoto@gmail.com>.
+ * Date: 5/14/15
+ * Time: 3:46 PM
+ * Powered By PhpStorm
+ */
 class Step
 {
 
