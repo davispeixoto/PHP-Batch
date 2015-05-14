f17e187bbbd1fe4558a71a0801b7f65d157dcaa2
Davis L P Peixoto <davis.peixoto@gmail.com>
2015-05-14 18:29:18 -0300

    item reader and writer interfaces

---
+++ src/Davispeixoto/PhpBatch/Contracts/ItemReaderInterface.php
@@ -0,0 +1,17 @@
+<?php namespace Davispeixoto\PhpBatch\Contracts;
+
+/**
+ * Interface ItemReaderInterface
+ * @package Davispeixoto\PhpBatch\Contracts
+ */
+
+/**
+ * Created by Davis Peixoto <davis.peixoto@gmail.com>.
+ * Date: 5/14/15
+ * Time: 6:27 PM
+ * Powered By PhpStorm
+ */
+interface ItemReaderInterface
+{
+
+}
